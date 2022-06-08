89707035f65055ddb3eb78c74a7c212501212b4b
a-cs <acarneirosousa@gmail.com>
2022-06-07 22:42:05 -0300

    creating count_args

--- src/utils/tokenizer.c
+++ src/utils/tokenizer.c
@@ -6,32 +6,52 @@
 /*   By: acarneir <acarneir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 00:10:45 by acarneir          #+#    #+#             */
-/*   Updated: 2022/06/07 01:19:27 by acarneir         ###   ########.fr       */
+/*   Updated: 2022/06/07 22:41:25 by acarneir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void	count_args(t_data *obj)
+{
+	int	i;
+
+	i = 0;
+	while (obj->input[i])
+	{
+		if (obj->input[i] == DOUBLE_QUOTES)
+			while (obj->input[i] && obj->input[i] != DOUBLE_QUOTES)
+				i++;
+		else if (obj->input[i] == SINGLE_QUOTES)
+			while (obj->input[i] && obj->input[i] != SINGLE_QUOTES)
+				i++;
+		else if (obj->input[i] == SPACE_VALUE)
+			while (obj->input[i] && obj->input[i] == SPACE_VALUE)
+				i++;
+		else if (ft_isalpha(obj->input[i]))
+		{
+			obj->args_num++;
+			while (obj->input[i] && obj->input[i] != SPACE_VALUE)
+				i++;
+		}
+		else
+			i++;
+		// printf("i = %d\n", i);
+	}
+}
+
 char	**tokenizer(t_data *obj)
 {
 	char	**args;
 
+	count_args(obj);
+	// printf("args_num: %d\n", obj->args_num);
+	// args = ft_calloc(obj->args_num + 1, sizeof(char *));
+	// split_args(obj, args);
 	args = ft_split(obj->input, ' ');
-	while (args[obj->args_num])
-		obj->args_num++;
+	// while (args[obj->args_num])
+	// 	obj->args_num++;
 	return (args);
 }
 
-// while (input[i])
-// {
-// 	if (input[i] == DOUBLE_QUOTES)
-// 	{
-// 		while (input[i+j])
-// 		{
-// 			if (input[i+j] == DOUBLE_QUOTES)
-// 			return (i, j);
-// 			j++;
-// 		}
-// 	}
-// 	i++;
-// }
+
