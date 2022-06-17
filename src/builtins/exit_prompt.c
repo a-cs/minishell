46b2f7687d7fd37a781681e78f1a1530bce3b2aa
Rodrigo Longhi <rodrigo.longhi@mercadolivre.com>
2022-06-17 18:50:27 -0300

    fixing norm

--- src/builtins/exit_prompt.c
+++ src/builtins/exit_prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit_prompt.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acarneir <acarneir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 22:39:43 by rfelipe-          #+#    #+#             */
-/*   Updated: 2022/05/12 22:25:25 by acarneir         ###   ########.fr       */
+/*   Updated: 2022/06/17 18:49:47 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	exit_prompt(t_data *obj)
 {
 	if (obj->input)
 		free(obj->input);
-    if (obj->envp)
-        ft_free_matrix(obj->envp);
+	if (obj->envp)
+		ft_free_matrix(obj->envp);
 	if (obj)
 		free(obj);
 	exit(0);
